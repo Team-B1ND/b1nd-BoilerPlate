029639e296cac555b729ddf50ce8d623aef7247d
해준 <127281057+ftery0@users.noreply.github.com>
2024-09-03 17:38:20 +0900

    refactor :: 필요없는 부분 삭제

--- src/App.tsx
+++ src/App.tsx
@@ -8,15 +8,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Bind AUTH-TEMPLATE
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/Team-B1ND/B1ND-AUTH-TEMPLATE"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          BindGithub
         </a>
       </header>
     </div>
