43db95c1ca6476c22074dc22fddcc408d623e265
ftery0 <lerb5253@dgsw.hs.kr>
2025-03-18 14:59:10 +0900

    feat :: webpack js

--- template/vite-ts/vite.config.ts
+++ template/vite-ts/vite.config.ts
@@ -5,16 +5,26 @@ import path from 'path';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'build',
+    sourcemap: false, 
+    minify: 'esbuild', 
+    rollupOptions: {
+      output: {
+        manualChunks(id: string | string[]) {
+          if (id.includes('node_modules')) {
+            return 'vendor'; 
+          }
+        },
+      },
+    },
+  },
   resolve: {
     //추가
     alias: [
       {
         find: '@src',
         replacement: path.resolve(__dirname, 'src'),
-      },
-      {
-        find: '@components',
-        replacement: path.resolve(__dirname, 'src/components'),
       },
     ],
   },
